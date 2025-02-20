292a87771b4adfa5eae2fc43a513ff313c34ffc5
BenBerol <benberol40@gmail.com>
2025-02-20 15:25:35 -0500

    Update elevator and pivot pid

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -70,11 +70,11 @@ public final class Constants {
     public final static double ENCODER_SCALAR = 4.43;
     public final static double HEIGHT_BUMP = 0.037;
 
-    public final static double kV = 0.075;
-    public final static double kG = 0.12;
-    public final static double kP = 1.4;
-    public final static double kI = 0.05;
-    public final static double kD = 0.2;
+    public final static double kV = 0.15;
+    public final static double kG = 0.15;
+
+    public final static double kP = 0.63;
+    public final static double kD = 0.25;
   }
 
   public final static class IntakeConstants {
@@ -89,7 +89,13 @@ public final class Constants {
     public static final double pivotRotationsToArmRotations = -16;
     // Starting point of arm when resting on hard stop
     public static final double pivotOffsetFromStart = 0.25; //36 degrees up 22.2 percent elevator to intake
-    public static final double kG = -0.03;
+    // kLeverage plus kG must be no less than -0.045 to keep falcon happy
+    public static final double kLeverage = 0.02;
+    public static final double kG = 0.02;
+
+    public final static double kP = 0.0001;
+    public final static double kI = 0.00005;
+    public final static double kD = 0.00025;
   }
   public final static class PneumaticsConstants {
     public final static int PNEUMATICS_HUB_ID = 21;
