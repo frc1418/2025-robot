063fbbfa4b054a24f581723c0891c94fe15abbfd
BenBerol <benberol40@gmail.com>
2025-02-20 17:16:35 -0500

    Update wheel constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -21,39 +21,39 @@ public final class Constants {
     public static final double FRONT_LEFT_ENCODER_OFFSET = 3.43+Math.PI;
     public static final double FRONT_LEFT_P = 0.00040465;
     public static final double FRONT_LEFT_D = 0;
-    public static final double FRONT_LEFT_KS = 0.17025;
-    public static final double FRONT_LEFT_KV = 2.7288;
-    public static final double FRONT_LEFT_KA = 0.34097;
+    public static final double FRONT_LEFT_KS = 0.21461;
+    public static final double FRONT_LEFT_KV = 2.8012;
+    public static final double FRONT_LEFT_KA = 0.7612;
 
     public static final int FRONT_RIGHT_ANGLE_ID = 5;
     public static final int FRONT_RIGHT_SPEED_ID = 6;
     public static final Translation2d FRONT_RIGHT_LOC = new Translation2d(wheelPos, -wheelPos);
     public static final double FRONT_RIGHT_ENCODER_OFFSET = 0.7968+Math.PI;
-    public static final double FRONT_RIGHT_P = 0.00080684;
+    public static final double FRONT_RIGHT_P = 0.00091514;
     public static final double FRONT_RIGHT_D = 0;
-    public static final double FRONT_RIGHT_KS = 0.22297;
-    public static final double FRONT_RIGHT_KV = 2.6277;
-    public static final double FRONT_RIGHT_KA = 0.38782;
+    public static final double FRONT_RIGHT_KS = 0.34839;
+    public static final double FRONT_RIGHT_KV = 2.7542;
+    public static final double FRONT_RIGHT_KA = 0.55833;
     
     public static final int BACK_LEFT_ANGLE_ID = 1;
     public static final int BACK_LEFT_SPEED_ID = 2;
     public static final Translation2d BACK_LEFT_LOC = new Translation2d(-wheelPos, wheelPos);
     public static final double BACK_LEFT_ENCODER_OFFSET = 2.1945+Math.PI;
-    public static final double BACK_LEFT_P = 0.0012982;
+    public static final double BACK_LEFT_P = 0.0015266;
     public static final double BACK_LEFT_D = 0;
-    public static final double BACK_LEFT_KS = 0.22297;
-    public static final double BACK_LEFT_KV = 2.5534;
-    public static final double BACK_LEFT_KA = 0.36435;
+    public static final double BACK_LEFT_KS = 0.19566;
+    public static final double BACK_LEFT_KV = 2.6945;
+    public static final double BACK_LEFT_KA = 0.57784;
 
     public static final int BACK_RIGHT_ANGLE_ID = 7;
     public static final int BACK_RIGHT_SPEED_ID = 8;
     public static final Translation2d BACK_RIGHT_LOC = new Translation2d(-wheelPos, -wheelPos);
     public static final double BACK_RIGHT_ENCODER_OFFSET = 1.042+Math.PI;
-    public static final double BACK_RIGHT_P = 0.00011772;
+    public static final double BACK_RIGHT_P = 0.0010266;
     public static final double BACK_RIGHT_D = 0;
-    public static final double BACK_RIGHT_KS = 0.21364;
-    public static final double BACK_RIGHT_KV = 2.6634;
-    public static final double BACK_RIGHT_KA = 0.19487;
+    public static final double BACK_RIGHT_KS = 0.23609;
+    public static final double BACK_RIGHT_KV = 2.7405;
+    public static final double BACK_RIGHT_KA = 0.56268;
     
     public static final SwerveDriveKinematics SWERVE_KINEMATICS = new SwerveDriveKinematics(
       FRONT_LEFT_LOC,
@@ -72,6 +72,7 @@ public final class Constants {
 
     public final static double kV = 0.15;
     public final static double kG = 0.15;
+    public final static double kCoral = 0.025;
 
     public final static double kP = 0.63;
     public final static double kD = 0.25;
